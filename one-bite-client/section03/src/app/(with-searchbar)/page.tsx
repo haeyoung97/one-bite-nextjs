a83cc3e6c184531f6ba7a74848162dd1b18f0007
Haeyoung Park <godud6379@naver.com>
2024-09-22 22:34:00 +0900

    feat: add server component docs and example

--- one-bite-client/section03/src/app/(with-searchbar)/page.tsx
+++ one-bite-client/section03/src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,13 @@
+import ClientComponent from "@/components/ClientComponent";
+import ServerComponent from "@/components/ServerComponent";
+
 export default function Home() {
-  return <div>인덱스 페이지</div>;
+  return (
+    <div>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
