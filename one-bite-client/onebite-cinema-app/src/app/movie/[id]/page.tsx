bf6e223f0a1115fb761d846f8dd077adc111c7f9
Haeyoung Park <godud6379@naver.com>
2024-09-25 23:11:11 +0900

    feat: add full route cache logic for practice

--- one-bite-client/onebite-cinema-app/src/app/movie/[id]/page.tsx
+++ one-bite-client/onebite-cinema-app/src/app/movie/[id]/page.tsx
@@ -1,5 +1,13 @@
+import movies from "@/mock/movies.json";
 import { MovieData } from "@/types";
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return movies.map((movie) => ({ id: `${movie.id}` }));
+}
 
 export default async function Page({
   params,
@@ -15,7 +23,10 @@ export default async function Page({
     { cache: "force-cache" }
   );
 
-  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  if (!response.ok) {
+    if (response.status === 404) notFound();
+    return <div>오류가 발생했습니다...</div>;
+  }
 
   const movieData: MovieData = await response.json();
 
