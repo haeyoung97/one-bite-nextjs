e116ea357ed18eb8259e8bf9ce3881072959a227
Haeyoung Park <godud6379@naver.com>
2024-09-26 22:15:34 +0900

    feat: add loading component for streaming practice

--- one-bite-client/onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
+++ one-bite-client/onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import MovieItem from "@/components/MovieItem";
 import { MovieData } from "@/types";
+import delay from "@/utils/delay";
 import style from "./page.module.css";
 
 export default async function Page({
@@ -7,6 +8,7 @@ export default async function Page({
 }: {
   searchParams: { q?: string };
 }) {
+  await delay(1500);
   /**
    * 새로운 데이터가 추가되거나 삭제되지 않기 때문에 "force-cache" 로 설정.
    * 다만, 데이터의 추가/삭제 기능이 추가된다면, ISR 동작으로 변경되면 좋을 것 같다.
