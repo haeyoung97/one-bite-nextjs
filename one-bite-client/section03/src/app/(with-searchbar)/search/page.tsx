e847c48a03c3e9054d6d118363386257985b6619
Haeyoung Park <godud6379@naver.com>
2024-09-26 22:09:45 +0900

    feat: add docs and loading component for streaming

--- one-bite-client/section03/src/app/(with-searchbar)/search/page.tsx
+++ one-bite-client/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import BookItem from "@/components/BookItem";
 import { BookData } from "@/types";
+import { delay } from "@/utils/delay";
 
 // 동적 함수를 사용하고 있기 때문에 동적 페이지로 설정된다.
 export default async function Page({
@@ -7,6 +8,7 @@ export default async function Page({
 }: {
   searchParams: { q?: string };
 }) {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
     { cache: "force-cache" }
